1283663cd7f44a7881dfcd241c8fd7a860327a06
Adrian Edu <adrian07pro1@gmail.com>
2025-09-03 11:36:37 -0500

    mi primer programa

--- miprimerprograma.py
+++ miprimerprograma.py
@@ -4,3 +4,27 @@ print("Velazquez")
 print("Aviles")
 
 print('"¡Hola, Mundo!"')
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+print("\\")
+
+print("Eduardo", "Adrian", "Velazquez", "Aviles")
+
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+print("Eduardo", end=" ")
+print("Adrian", end=" ")
+print("Velazquez", end=" ")
+print("Aviles")
+
+print("Eduardo", "Adrian", "Velazquez", "Aviles", sep='-')
+
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
