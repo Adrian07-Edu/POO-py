3b4471829cfd6c6eb8ec76fdbf406521f9ced6e3
Adrian Edu <adrian07pro1@gmail.com>
2025-09-04 11:45:53 -0500

    operadores

---
+++ operadores.py
@@ -0,0 +1,30 @@
+a = 3
+b = 6
+c = a + b
+print(c)
+
+d = c * b
+print(d)
+
+e = d / a
+print(e)
+
+f = e - b
+print(f)
+
+g = f // a
+print(g)
+
+h = d ** g
+print(h)
+
+i = h % 4
+print(i)
+
+
+john = 3
+mary = 5
+adam = 6
+print(john, mary, adam)
+total_apples = john + mary + adam
+print(total_apples)
