bf3bb0d63505fbe4825c5c1b0e8aa2bb80b87782
Adrian Edu <adrian07pro1@gmail.com>
2025-09-04 11:46:07 -0500

    practica 1

---
+++ P1.py
@@ -0,0 +1,229 @@
+x = 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x ",x,"=",1*x)
+x = x + 1
+print("1 x", x,"=",1*x)
+
+print()
+
+x = 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x ",x,"=",2*x)
+x = x + 1
+print("2 x", x,"=",2*x)
+
+print()
+
+x = 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x ",x,"=",3*x)
+x = x + 1
+print("3 x", x,"=",3*x)
+
+print()
+
+x = 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x ",x,"=",4*x)
+x = x + 1
+print("4 x", x,"=",4*x)
+
+print()
+
+x = 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x ",x,"=",5*x)
+x = x + 1
+print("5 x", x,"=",5*x)
+
+print()
+
+x = 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("6 x ",x,"=",6*x)
+x = x + 1
+print("5 x", x,"=",6*x)
+
+print()
+
+x = 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x ",x,"=",7*x)
+x = x + 1
+print("7 x", x,"=",7*x)
+
+print()
+
+x = 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x ",x,"=",8*x)
+x = x + 1
+print("8 x", x,"=",8*x)
+
+print()
+
+x = 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x ",x,"=",9*x)
+x = x + 1
+print("9 x", x,"=",9*x)
+
+print()
+
+x = 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x ",x,"=",10*x)
+x = x + 1
+print("10 x", x,"=",10*x)
+
+
