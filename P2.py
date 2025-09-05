01ec90e401e1c4bef1b1fec9934334ed6b4a22ac
Adrian Edu <adrian07pro1@gmail.com>
2025-09-05 09:30:21 -0500

    practica 2

---
+++ P2.py
@@ -0,0 +1,18 @@
+matematicas = 10
+ingles = 8
+educacion_fisica = 7
+biologia = 9
+
+suma_calificaciones = matematicas + ingles + educacion_fisica + biologia
+promedio = suma_calificaciones / 4
+
+print("+------------------------+")
+print("|     Calificaciones     |")
+print("+------------------+-----+")
+print("| Educacion Fisica |", educacion_fisica, '  |')
+print("|      Matematicas |", matematicas, ' |')
+print("|         Biologia |", biologia, '  |')
+print("|           Ingles |", ingles, '  |')
+print("+------------------+-----+")
+print("|         Promedio |", promedio, '|')
+print("+------------------+-----+")
