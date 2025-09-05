7489c4fb1ffad4b87e3eccb49ce19225e7ff46aa
Adrian Edu <adrian07pro1@gmail.com>
2025-09-05 09:30:09 -0500

    practica 2

---
+++ P3.py
@@ -0,0 +1,18 @@
+pesos = 826
+dolares = 42
+
+# dolar en pesos
+tasa_cambio = 18.63
+# pesos en dolares
+tasa_cambio_inv = round(1 / tasa_cambio, 3)
+
+dolares_pesos = round(dolares * tasa_cambio, 2)
+pesos_dolares = round(pesos / tasa_cambio, 2)
+
+print("+-----------+-------------+-------------+")
+print("|           |   Dolares   |    Pesos    |")
+print("+-----------+-------------+-------------+")
+print("|      Tasa |", tasa_cambio, 'US-MX |', tasa_cambio_inv, 'MX-US |')
+print("|  Cantidad | US$", dolares, '     | MX$', pesos, '    |')
+print("| Resultado | MX$", dolares_pesos, ' | US$', pesos_dolares, '  |')
+print("+-----------+-------------+-------------+")
