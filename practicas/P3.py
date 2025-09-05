aed252e1a2f49a6970cac16e1726804ecdf144aa
Adrian Edu <adrian07pro1@gmail.com>
2025-09-05 09:33:46 -0500

    comentarios

--- practicas/P3.py
+++ practicas/P3.py
@@ -2,9 +2,10 @@ pesos = 826
 dolares = 42
 
 # dolar en pesos
-tasa_cambio = 18.63
+tasa_cambio = 18.63 # dolar / pesos
 # pesos en dolares
-tasa_cambio_inv = round(1 / tasa_cambio, 3)
+tasa_cambio_inv = round(1 / tasa_cambio, 3) # peso / dolar
+# 1 / (dolar / pesos) = peso / dolar
 
 dolares_pesos = round(dolares * tasa_cambio, 2)
 pesos_dolares = round(pesos / tasa_cambio, 2)
