c3eab5394875fc0046acb17cf82075f64ee575d0
Adrian Edu <adrian07pro1@gmail.com>
2025-09-08 11:58:04 -0500

    t

---
+++ practicas/P4.py
@@ -0,0 +1,32 @@
+suma_v1=int(input('Ingresa el primer valor de la suma: '))
+suma_v2=int(input('Ingresa el segundo valor de la suma: '))
+suma_r = suma_v1 + suma_v2
+print("Resultado de la suma:", suma_r)
+print()
+
+resta_v1=int(input('Ingresa el primer valor de la resta: '))
+resta_v2=int(input('Ingresa el segundo valor de la resta: '))
+resta_r = resta_v1 - resta_v2
+print('Resultado de la resta', resta_r)
+print()
+
+mul_v1=int(input('Ingresa el primer valor de la multiplicación: '))
+mul_v2=int(input('Ingresa el segundo valor de la multiplicación: '))
+mul_r = mul_v1 * mul_v2
+print('Resultado de la multiplicación', mul_r)
+print()
+
+div_v1=int(input('Ingresa el primer valor de la división: '))
+div_v2=int(input('Ingresa el segundo valor de la división: '))
+div_r = div_v1 / div_v2
+print('Resultado de la división', div_r)
+print()
+
+print()
+print('-------------------')
+print('--- Operaciones ---')
+print('-------------------')
+print(suma_v1, '+', suma_v2, '=', suma_r)
+print(resta_v1, '-', resta_v2, '=', resta_r)
+print(mul_v1, '*', mul_v2, '=', mul_r)
+print(div_v1, '/', div_v2, '=', div_r)
---
+++ practicas/P5.py
@@ -0,0 +1,5 @@
+actual = 2025
+nacimiento = int(input('Ingresa tu año de nacimiento: '))
+edad = actual - nacimiento
+
+print('Tu edad es:', edad, 'años')
