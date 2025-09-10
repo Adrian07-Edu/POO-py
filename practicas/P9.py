7548f6a178489d2111845ea1e3bcfb34a4dd43a5
Adrian Edu <adrian07pro1@gmail.com>
2025-09-10 09:44:54 -0500

    edit P9

--- practicas/P9.py
+++ practicas/P9.py
@@ -1,6 +1,5 @@
 color = input("Introduce un color (en minusculas): ")
 
-valido = True
 traduccion = ''
 
 if color == 'amarillo': traduccion = 'yellow'
@@ -8,7 +7,6 @@ elif color == 'rojo': traduccion = 'red'
 elif color == 'azul': traduccion = 'blue'
 elif color == 'verde': traduccion = 'green'
 elif color == 'blanco': traduccion = 'white'
-else: valido = False
 
-if valido: print('La traducción de', color, 'es', traduccion)
+if traduccion != '': print('La traducción de', color, 'es', traduccion)
 else: print('El color', color, 'no es válido')
