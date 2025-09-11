266981c70fb037c19836b6c4a6b566c52a755f90
Adrian Edu <adrian07pro1@gmail.com>
2025-09-11 11:43:05 -0500

    cpu

--- n.py
+++ n.py
@@ -1,23 +1,6 @@
-from random import randint
+while True:
+    palabra = input("Introduce una palabra: ")
+    if palabra == "chupacabra":
+        break
 
-secret_number = randint(1, 100)
-print(
-"""
-+================================+
-| ¡Bienvenido a mi juego, muggle!|
-| Introduce un número entero     |
-| y adivina qué número he        |
-| elegido para ti.               |
-|¿Cuál es el número secreto?     |
-+================================+
-""")
-selected_number = int(input("Introduce un número: "))
-tries = 1 
-while selected_number != secret_number:
-    if selected_number < secret_number:
-        print("¡Ja, ja! ¡Estás atrapado en mi bucle! El número es mayor.")
-    else:
-        print("¡Ja, ja! ¡Estás atrapado en mi bucle! El número es menor.")
-    selected_number = int(input("Introduce un número: "))
-    tries += 1
-print("¡Bien hecho, muggle! Eres libre ahora. Intentos:", tries)
+print("Has dejado el bucle con éxito.")
