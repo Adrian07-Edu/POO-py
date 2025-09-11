4db2f2af615eb5cba9cd1c6fc4a2c19250cd008b
Adrian Edu <adrian07pro1@gmail.com>
2025-09-11 09:11:10 -0500

    practica 11

--- practicas/P11.py
+++ practicas/P11.py
@@ -1,4 +1,5 @@
 secret_number = 777
+
 print(
 """
 +================================+
@@ -9,8 +10,11 @@ print(
 |¿Cuál es el número secreto?     |
 +================================+
 """)
+
 selected_number = int(input("Introduce un número: "))
+
 while selected_number != secret_number:
     print("¡Ja, ja! ¡Estás atrapado en mi bucle!")
     selected_number = int(input("Introduce un número: "))
+
 print("¡Bien hecho, muggle! Eres libre ahora.")
