10410994ee0647fc79ecc99450a36e52c7ec5642
Adrian Edu <adrian07pro1@gmail.com>
2025-09-12 11:08:52 -0500

    reposicion

--- devorador.py
+++ devorador.py
@@ -1,9 +1,9 @@
+palabra = input("Introduce una palabra: ").upper()
+
 # Forma corta - (No sigue las especificaciones del problema). Utiliza el operador 'in' y 'not'
 # for letra in palabra:
-#     if not letra in "AEIOU":
-#         print(letra)
+#     if not letra in "AEIOU": print(letra)
 
-palabra = input("Introduce una palabra: ").upper()
 for letra in palabra:
     if letra == "A": continue
     elif letra == "E": continue
