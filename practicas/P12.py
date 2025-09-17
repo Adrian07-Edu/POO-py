26b0b698b29fb267404bd4735acaa698dcad15fa
Adrian Edu <adrian07pro1@gmail.com>
2025-09-17 10:16:29 -0500

    practicas

---
+++ practicas/P12.py
@@ -0,0 +1,6 @@
+while True:
+    palabra = input("Introduce una palabra: ")
+    if palabra == "chupacabra":
+        break
+
+print("Has dejado el bucle con éxito.")
---
+++ practicas/P13.py
@@ -0,0 +1,8 @@
+hat_list = [1, 2, 3, 4, 5]
+hat_list[2] = int(input("introduce un numero: "))
+
+del hat_list[-1]
+
+print(len(hat_list))
+
+print(hat_list)
