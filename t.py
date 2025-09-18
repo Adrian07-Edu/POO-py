68afd10075fba596a93bab4e33ca29e3f8223b36
Adrian Edu <adrian07pro1@gmail.com>
2025-09-18 11:51:58 -0500

    beattles

--- t.py
+++ t.py
@@ -1,10 +1,28 @@
-numeros=[2,4,6,8,10,12,14,16]
-print(len(numeros))
-numeros[5] = 50
-print(numeros)
-numeros[2] = numeros[-1]
-print(numeros)
-del numeros[3]
-print(numeros)
-numeros[1] = 'Hola'
-print(numeros)
+# paso 1
+beatles = []
+print("Paso 1:", beatles)
+
+# paso 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Paso 2:", beatles)
+
+# paso 3
+for nombre in ['Stuart', 'Pete']:
+    beatle = input("Introduce el nombre de " + nombre + ": ")
+    beatles.append(beatle)
+print("Paso 3:", beatles)
+
+# paso 4
+del beatles[-2]
+del beatles[-1]
+print("Paso 4:", beatles)
+
+# paso 5
+beatles.insert(0, "Ringo Starr")
+print("Paso 5:", beatles)
+
+
+# probando la longitud de la lista
+print("Los Fav", len(beatles))
