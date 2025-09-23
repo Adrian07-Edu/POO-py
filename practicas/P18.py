e82c1a6cbf894e85b4558a5ca0c5ff1216fd6379
Adrian Edu <adrian07pro1@gmail.com>
2025-09-23 11:55:14 -0500

    lens

--- practicas/P18.py
+++ practicas/P18.py
@@ -1,7 +1,7 @@
 vocales = []
 consonantes = []
 len_consonantes, len_vocales = 0, 0
-while len(vocales) + len(consonantes) < 10:
+while len_vocales + len_consonantes < 10:
     letra = input("Introduce una letra: ")
     longitud = len(letra)
     if longitud == 0: continue
