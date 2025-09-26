dfc9fbfaa45b8a209c8a151b15990378db8c64ab
Adrian Edu <adrian07pro1@gmail.com>
2025-09-26 11:42:53 -0500

    suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+def suma(valor1, valor2):
+    print(valor1, '+', valor2, '=', valor1 + valor2)
+
+valor1 = int(input('ingresa el primer valor: '))
+valor2 = int(input('ingresa el segundo valor: '))
+
+suma(valor1, valor2)
