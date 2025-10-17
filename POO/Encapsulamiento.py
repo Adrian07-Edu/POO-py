3ded0508c89c4078c4474bff46583380ed01540c
Adrian Edu <adrian07pro1@gmail.com>
2025-10-17 10:39:07 -0600

    encapsulamiento

---
+++ POO/Encapsulamiento.py
@@ -0,0 +1,28 @@
+class A:
+    def __init__(self):
+        self.contador = 0
+    def incrementar(self):
+        self.contador += 1
+    def cuenta(self):
+        return self.contador
+
+class B:
+    def __init__(self):
+        self.__contador = 0
+    def incrementar(self):
+        self.__contador += 1
+    def cuenta(self):
+        return self.__contador
+
+print('objeto A')
+a = A()
+print(a.cuenta())
+a.incrementar()
+print(a.cuenta())
+print(a.contador)
+
+print('objeto B')
+b = B()
+print(b.cuenta())
+b.incrementar()
+print(b.cuenta())
